2321345d420cbf3b8dfe5d739c9d3d4bc8f6fafe
Brenda Z <brenda.zamarripa5773@alumnos.udg.mx>
2024-09-22 16:45:40 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        beige: "#e9d8a6",
+        black: "#001219",
+        cherry: "#9b2226",
+        salmon: "#e76f51",
+        perri: "#2a9d8f",
+        mostaza: "#ee9b00",
+        skin: "#f4a261",
+        ocean: "#264653",
+        yellow: "#ddb166",
+        mandarina: "#fe8626",
+        rosa: "#feabab",
+        ladrillo: "#ce6e44",
+        naranjoso: "#feb23c",
+        durazno: "#fedab6",
+      },
+      fontFamily: {
+        lobster: ["Lobster", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
